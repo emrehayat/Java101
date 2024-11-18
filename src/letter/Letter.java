3b3b5d706dfe96bb6b3dde551ae1552760fb2000
Emre <emrhayat1905@gmail.com>
2024-11-19 00:09:53 +0300

    letter b

---
+++ src/letter/Letter.java
@@ -0,0 +1,28 @@
+package letter;
+
+public class Letter {
+    public static void main(String[] args) {
+        String[][] letter = new String[7][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 6) {
+                    letter[i][j] = " * ";
+                } else if (j == 0) {
+                    letter[i][j] = " * ";
+                } else if ((i == 1 && j == 3) || (i == 2 && j == 2) || (i == 3 && j == 1) || (i == 4 && j == 2) || (i == 5 && j == 3)) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : letter) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
